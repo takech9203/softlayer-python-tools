0bf7fedbcd44bcd9a1d347ab1a6dfc9b18f58e0d
takechika <takech@9203.net>
2015-01-31 12:41:01 +0900

    first release

---
+++ sl_getAutoScaleGroup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'takechika'
+
+from prettytable import PrettyTable
+import SoftLayer
+import sluser
+
+SL_USERNAME = sluser.SL_USERNAME
+SL_API_KEY = sluser.SL_API_KEY
+
+nsAutoScaleGroup = 'TK-as-group'
+
+_maskAutoScaleGroup = '''
+    virtualGuestMemberCount
+    '''
+
+client = SoftLayer.Client(username=SL_USERNAME, api_key=SL_API_KEY)
+autoScaleGroups = client['Account'].getScaleGroups(mask=_maskAutoScaleGroup)
+
+_tableHeader = [
+    'id',
+    'asg name',
+    'dc',
+    'status',
+    'min',
+    'max',
+    'now',
+    'member prefix',
+    'members',
+    ]
+
+# Table definition
+table = PrettyTable(_tableHeader)
+table.padding_width = 1
+
+for asg in autoScaleGroups:
+
+    m = ""
+    ms = ""
+    for a in asg['virtualGuestMembers'][0:]:
+#        m =  str(a['virtualGuest']['id']) + ":" + a['virtualGuest']['hostname'][-4:]
+        m =  a['virtualGuest']['hostname'][-4:]
+        ms = m + ' ' + ms
+
+    table.add_row(
+        [
+            asg['id'],
+            asg['name'],
+            asg['virtualGuestMemberTemplate']['datacenter']['name'],
+            asg['status']['keyName'],
+            asg['minimumMemberCount'],
+            asg['maximumMemberCount'],
+            asg['virtualGuestMemberCount'],
+            asg['virtualGuestMemberTemplate']['hostname'],
+            ms,
+        ]
+    )
+
+print(table)
+print("members: prefix-xxxx")
+
+exit()
